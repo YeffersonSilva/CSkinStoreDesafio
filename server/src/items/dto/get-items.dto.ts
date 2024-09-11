446130ede6dfeb4c5334a9c04560ea8a2d1f206a
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-09-10 21:41:28 -0300

    feat: Add documentation properties to GetItemsDto class
    - Enhanced the `GetItemsDto` class with documentation properties.- Added Swagger decorators to specify metadata and improve API documentation for filtering and sorting items.

--- server/src/items/dto/get-items.dto.ts
+++ server/src/items/dto/get-items.dto.ts
@@ -7,6 +7,7 @@ import {
   IsEnum,
 } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 enum OrderBy {
   price = 'price',
@@ -19,10 +20,17 @@ enum Order {
 }
 
 export class GetItemsDto {
+  @ApiProperty({ required: false, description: 'Filter by item name' })
   @IsOptional()
   @IsString()
   name?: string;
 
+  @ApiProperty({
+    required: false,
+    minimum: 0,
+    maximum: 1,
+    description: 'Minimum float value',
+  })
   @IsOptional()
   @IsNumber()
   @Min(0)
@@ -30,6 +38,12 @@ export class GetItemsDto {
   @Type(() => Number)
   minFloat?: number;
 
+  @ApiProperty({
+    required: false,
+    minimum: 0,
+    maximum: 1,
+    description: 'Maximum float value',
+  })
   @IsOptional()
   @IsNumber()
   @Min(0)
@@ -37,26 +51,35 @@ export class GetItemsDto {
   @Type(() => Number)
   maxFloat?: number;
 
+  @ApiProperty({ required: false, minimum: 0, description: 'Minimum price' })
   @IsOptional()
   @IsNumber()
   @Min(0)
   @Type(() => Number)
   minPrice?: number;
 
+  @ApiProperty({ required: false, minimum: 0, description: 'Maximum price' })
   @IsOptional()
   @IsNumber()
   @Min(0)
   @Type(() => Number)
   maxPrice?: number;
 
+  @ApiProperty({ required: false, description: 'Filter by category' })
   @IsOptional()
   @IsString()
   category?: string;
 
+  @ApiProperty({
+    required: false,
+    enum: OrderBy,
+    description: 'Order by field',
+  })
   @IsOptional()
   @IsEnum(OrderBy)
   orderBy?: OrderBy;
 
+  @ApiProperty({ required: false, enum: Order, description: 'Sort order' })
   @IsOptional()
   @IsEnum(Order)
   order?: Order;
