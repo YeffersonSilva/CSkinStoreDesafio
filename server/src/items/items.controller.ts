203b9a7aa0c6021aae498d13e8ec20f8630e69cd
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-09-11 01:17:05 -0300

    refactor: Simplify items controller
    The items controller has been simplified by removing the getItemById method, as it is not used in the current code.

--- server/src/items/items.controller.ts
+++ server/src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { GetItemsDto } from './dto/get-items.dto';
 
@@ -7,12 +7,7 @@ export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
   @Get()
-  getItems(@Query() getItemsDto: GetItemsDto) {
+  async getItems(@Query() getItemsDto: GetItemsDto) {
     return this.itemsService.getItems(getItemsDto);
-  }
-
-  @Get(':id')
-  getItemById(@Param('id') id: string) {
-    return this.itemsService.getItemById(id);
   }
 }
