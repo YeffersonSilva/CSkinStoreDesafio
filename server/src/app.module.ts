171b148aead352efe69f30c1eaac83a15d21f6b6
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-09-11 01:17:16 -0300

    refactor: Simplify app.module.ts imports and remove unused code

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ItemsModule } from './items/items.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { ItemsModule } from './items/items.module'; // Importa los módulos que necesites
 
 @Module({
-  imports: [ItemsModule, PrismaModule],
+  imports: [ItemsModule], // Agrega aquí tus módulos
+  controllers: [], // Si tienes controladores
+  providers: [], // Si tienes servicios
 })
 export class AppModule {}
