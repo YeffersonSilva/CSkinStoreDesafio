5405ba2469201c184736c069a5aaf84468d61fd2
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-09-11 21:42:08 -0300

    feat: Agregar componente Header para mejorar el diseño

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
+import Link from 'next/link'; // Importa Link de next/link
+
+const Header: React.FC = () => {
+  return (
+    <Box bg="black" py={4}> {/* Cambia el fondo a negro */}
+      <Flex as="header" align="center" justify="center" maxW="container.xl" mx="auto" px={4}>
+        <Link href="/" passHref>
+          <Image 
+            src="https://pbs.twimg.com/profile_images/1721297694113599489/rzTCY03o_400x400.jpg" 
+            alt="CS:GO Skins" 
+            boxSize="50px" 
+            mr={4} 
+          />
+        </Link>
+        <Text fontSize="2xl" fontWeight="bold" color="white" textAlign="center">
+          <span style={{ color: 'white' }}>CSKin </span>
+          <span style={{ color: 'orange' }}>Store</span>
+        </Text>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Header;
